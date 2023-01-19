a12e15a443535217768068449caa0d1ab8d822d2
Christopher Maddalena <chris.maddalena@protonmail.com>
2023-01-19 13:17:02 -0800

    Added flag for token with `argparse`

--- run.py
+++ run.py
@@ -1,32 +1,33 @@
+import argparse
 import json
 import os
-import sys
 
 from lib.builder import ReadMeBuilder
 from lib.extractor import DataExtractor
 from lib.loader import load_config
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("command", type=str, help="The action you want to run: fetch, build, update, all")
+    parser.add_argument("-t", "--token", help="GitHub Personal Access Token")
+    args = parser.parse_args()
+
     script_path = os.path.realpath(".")
     config_path = os.path.join(script_path, "config", "config.yml")
 
     cfg = load_config(config_path)
     output_path = os.path.join(script_path, "output", cfg["github"]["output"])
 
-    try:
-        command = sys.argv[1]
-    except IndexError:
-        print("Please provide a command: fetch, build, all")
-        raise SystemExit
-
-    if command not in ["fetch", "build", "update", "all"]:
-        print("Please provide a valid command: fetch, build, update, all")
-        raise SystemExit
-
+    command = args.command
     if command in ["fetch", "all"]:
-        extractor = DataExtractor()
-        extractor.fetch()
-        extractor.dump_json()
+        if args.token:
+            token = args.token
+            extractor = DataExtractor(token=token)
+            extractor.fetch()
+            extractor.dump_json()
+        else:
+            print("Please provide a GitHub Personal Access Token with the `-t` flag")
+            raise SystemExit
 
     if command in ["build", "all"]:
         with open(output_path, "r") as f:
@@ -36,6 +37,6 @@ if __name__ == "__main__":
         builder.build()
 
     if command == "update":
-        extractor = DataExtractor(skip_tests=True)
+        extractor = DataExtractor(local_only=True)
         extractor.update()
         extractor.dump_json()
